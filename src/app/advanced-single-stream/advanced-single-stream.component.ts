7c070108c6c7b1e35ca3bac41da9ce7dcc50e1d1
Sputnavi <sputnavik@yandex.by>
2023-02-26 13:36:04 +0300

    Finish third component

--- src/app/advanced-single-stream/advanced-single-stream.component.ts
+++ src/app/advanced-single-stream/advanced-single-stream.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, take, map, filter, of, delay, repeat } from 'rxjs';
 
 @Component({
   selector: 'app-advanced-single-stream',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./advanced-single-stream.component.css']
 })
 export class AdvancedSingleStreamComponent {
+  run1(): void {
+    const numbers = interval(1000).pipe(take(20));
+    const subscribe = numbers.subscribe(x => of(x).pipe(delay(200),repeat(1000)).subscribe(x => console.log(x)));
+  }
 
+  run2(): void {
+    const numbers = interval(1000).pipe(take(20));
+    const tookNumbers = numbers.pipe(take(7));
+    const subscribe = tookNumbers.subscribe(x => console.log(x));
+  }
+
+  run3(): void {
+    const numbers = interval(1000).pipe(take(20));
+    const evenNumbers = numbers.pipe(filter(x => x % 2 == 0));
+    const subscribe = evenNumbers.subscribe(x => console.log(x));
+  }
+
+  run4(): void {
+    const numbers = interval(1000).pipe(take(20));
+    const evenNumbers = numbers.pipe(filter(x => x % 2 == 0));
+    const subscribe = evenNumbers.subscribe(x => console.log(x));
+  }
 }
--- src/app/multiple-streams/multiple-streams.component.ts
+++ src/app/multiple-streams/multiple-streams.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, take, map, filter, combineAll, zip, combineLatest, last } from 'rxjs';
 
 @Component({
   selector: 'app-multiple-streams',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./multiple-streams.component.css']
 })
 export class MultipleStreamsComponent {
+  run1(): void {
+    const source1 = interval(200).pipe(take(10),map((val) => 'source1 val = ' + val));
+    const source2 = interval(300).pipe(take(10),map((val) => 'source2 val = ' + val));
+    const source3 = interval(400).pipe(take(10),map((val) => 'source3 val = ' + val));
 
+    const combined = combineLatest([source1, source2, source3]);
+
+    const subscribe = combined.subscribe(([x1, x2, x3]) => console.log(x1, x2, x3));
+  }
+
+  run2(): void {
+    const source1 = interval(200).pipe(take(10),map((val) => 'source1 val = ' + val));
+    const source2 = interval(300).pipe(take(10),map((val) => 'source2 val = ' + val));
+    const source3 = interval(400).pipe(take(10),map((val) => 'source3 val = ' + val));
+
+    const last1 = source1.pipe(last());
+    const last2 = source2.pipe(last());
+    const last3 = source3.pipe(last());
+
+    const combined = zip([last1, last2, last3]);
+
+    const subscribe = combined.subscribe(([x1, x2, x3]) => console.log(x1, x2, x3));
+  }
+
+  run3(): void {
+    const source1 = interval(200).pipe(take(10),map((val) => 'source1 val = ' + val));
+    const source2 = interval(300).pipe(take(10),map((val) => 'source2 val = ' + val));
+    const source3 = interval(400).pipe(take(10),map((val) => 'source3 val = ' + val));
+
+    const combined = zip(source1, source2, source3);
+
+    const subscribe = combined.subscribe(x => console.log(x));
+  }
 }
